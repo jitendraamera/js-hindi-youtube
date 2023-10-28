2a2e5890a8c42f2fd98ad74cd3fcf9e4c00b5411
jitendraamera <148367244+jitendraamera@users.noreply.github.com>
2023-10-28 05:05:03 +0000

    array complete

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,46 @@
+const myArr = [0, 1, 2, 3, 4, 5];
+// console.log(myArr[3]);
+
+// const myHeros = ['balveer', 'Ram', 'maharana'];
+// console.log(myHeros[1]);
+
+// Array Methods
+
+// push method add value in last index
+
+// myArr.push(7); 
+// myArr.push(8);
+
+// Pop method remove value in last index
+// myArr.pop();
+
+// unshift method add value in first index
+// myArr.unshift(9)
+
+// shift remove value in first index
+// myArr.shift()
+// console.log(myArr);
+
+// includes method output boolean return krta h
+// console.log(myArr.includes(10));
+
+// index of method output agr value h toh index number or nhi toh -1 return karega
+// console.log(myArr.indexOf(3));
+
+// Join method one more array join in string
+
+// const newArr = myArr.join();
+// console.log(myArr);
+// console.log(newArr);
+
+// slice, splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3);
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3);
+console.log(myn2);
+
---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,31 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"];
+const dc_heros = ["superman", "flash", "batman"];
+
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][0]);
+
+// const all_heros = marvel_heros.concat(dc_heros);
+// console.log(all_heros);
+
+// spread operater join the array
+// const all_new_heros = [...marvel_heros, ...dc_heros]
+// console.log(all_new_heros);
+
+// const another_array = [1, 2, 3, [4, 5, 7], 8, [7, [8, 9]]];
+// console.log(another_array)
+//  const real_another_array = another_array.flat(Infinity);
+//  console.log(real_another_array); 
+
+
+//  console.log(Array.isArray('jitsa'));
+// conver in array
+// console.log(Array.from('jitsa'));
+// console.log(Array.from({name: 'ram'}));
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3))
+
