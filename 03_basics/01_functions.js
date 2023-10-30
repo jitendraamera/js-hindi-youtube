d249ed0c91f0f74743bcf92688c77056f75eeee0
jitendraamera <148367244+jitendraamera@users.noreply.github.com>
2023-10-30 12:11:20 +0000

    fun

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,27 @@
+function myName(){
+    console.log("R");
+    console.log("a");
+    console.log("m");
+}
+
+// myName();
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+// addTwoNumbers(3, 5);
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2;
+    // return result;
+
+    return number1 + number2;
+}
+
+const result = addTwoNumbers(3, 5);
+console.log("result: ", result)
+
+/*
+ Return k bad kuch b print nhi hota h
+*/ 
