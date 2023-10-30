929b31a84262a96e2802beaa26257a96aad7eccb
jitendraamera <148367244+jitendraamera@users.noreply.github.com>
2023-10-30 06:29:16 +0000

    objects second video complete

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,41 @@
+const tinderUser = new Object(); // Singleton Objects 
+// const tinderUser ={}; // Non Singleton Objects
+ 
+tinderUser.id = "123abc";
+tinderUser.name = "Sammy";
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+// Object Nesting
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userFullName: {
+            firstName: "Ram",
+            lastName: "singh"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userFullName.firstName);
+
+// concat to one more array
+
+const obj1 = {1: 'a', 2: 'b'};
+const obj2 = {3: 'c', 4: 'd'};
+const obj4 = {5: 'e', 7: 'f'};
+
+// const obj3 = {obj1, obj2};
+// const obj3 = Object.assign({}, obj1, obj2, obj4);
+const obj3 = { ...obj1, ...obj2, ...obj4 };
+
+// console.log(obj3)
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('name')); // Output boolean type m aata h 
