e705146c72dfef10389f939e7f1189a4440d211a
jitendraamera <148367244+jitendraamera@users.noreply.github.com>
2023-11-10 08:53:53 +0000

    control flow folder

---
+++ 04_control_flow/one.js
@@ -0,0 +1,52 @@
+
+//  if
+const isuserloggedIn = true;
+const temperature = 41;
+
+if(2==="2"){
+    console.log("executed");
+}
+
+// if(temperature < 50){
+//     console.log("Temperature is less than 50");
+// }
+// else{
+//     console.log("Temperature is greater than 50")
+// }
+
+// <, >, <=, >=, ==, !=, ===
+
+// const score = 200;
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User power ${power}`)
+// }
+// console.log(`User power ${power}`)
+
+// const balance = 1000;
+// if(balance > 500) console.log("test"),console.log("test2");
+
+// if(balance < 500){
+//     console.log("Less than 500");
+// } else if(balance < 750){
+//     console.log("Less than 750");
+// } else if(balance < 900){
+//     console.log("Less than 900");
+// } else{
+//     console.log("Less than 1200")
+// }
+
+
+const userLogedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+// if(userLogedIn && debitCard){
+//     console.log("Allow to buy course");
+// }
+
+// if(loggedInFromGoogle || loggedInFromEmail){
+//     console.log("User Logged In");
+// }
